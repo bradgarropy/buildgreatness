f83c6fef2e709d98ae31031b36381a0fbbc8020b
Brad Garropy <bradgarropy@gmail.com>
2018-02-27 21:47:05 -0600

    debug log.

--- src/api/users.js
+++ src/api/users.js
@@ -3,6 +3,7 @@ import axios from "axios"
 
 export function registerUser(data) {
 
-    return axios.post(`${process.env.REACT_APP_API_URL}/users`, data)
+    console.log(process.env.REACT_APP_API_URL)
+    return axios.post("process.env.REACT_APP_API_URL/users", data)
 
 }
