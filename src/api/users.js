b58372dea1579262034a2a65ae07e82c41d53077
Brad Garropy <bradgarropy@gmail.com>
2018-02-27 22:14:27 -0600

    debug log.

--- src/api/users.js
+++ src/api/users.js
@@ -5,6 +5,13 @@ export function registerUser(data) {
 
     // eslint-disable-next-line
     console.log(process.env.REACT_APP_API_URL)
+
+    // eslint-disable-next-line
+    console.log(`${process.env.REACT_APP_API_URL}`)
+
+    // eslint-disable-next-line
+    console.log(`${process.env.REACT_APP_API_URL}/users`)
+
     return axios.post(`${process.env.REACT_APP_API_URL}/users`, data)
 
 }
