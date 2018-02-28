c8bfa7506a6f12a16da0915cfc2499c30e2e06ec
Brad Garropy <bradgarropy@gmail.com>
2018-02-27 21:50:16 -0600

    removing eslint.

--- src/api/users.js
+++ src/api/users.js
@@ -3,6 +3,7 @@ import axios from "axios"
 
 export function registerUser(data) {
 
+    // eslint-disable-next-line
     console.log(process.env.REACT_APP_API_URL)
     return axios.post("process.env.REACT_APP_API_URL/users", data)
 
