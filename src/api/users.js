d93600b6b6331755b2900e07d4affadc060b5a26
Brad Garropy <bradgarropy@gmail.com>
2018-02-27 22:01:10 -0600

    string literal.

--- src/api/users.js
+++ src/api/users.js
@@ -5,6 +5,6 @@ export function registerUser(data) {
 
     // eslint-disable-next-line
     console.log(process.env.REACT_APP_API_URL)
-    return axios.post("process.env.REACT_APP_API_URL/users", data)
+    return axios.post(`${process.env.REACT_APP_API_URL}/users`, data)
 
 }
