07ae0234c63232c7849b6b69ee3fe9f4d68016ee
Brad Garropy <bradgarropy@gmail.com>
2018-02-27 22:18:54 -0600

    debug log.

--- src/api/users.js
+++ src/api/users.js
@@ -4,6 +4,9 @@ import axios from "axios"
 export function registerUser(data) {
 
     // eslint-disable-next-line
+    console.log(process.env)
+
+    // eslint-disable-next-line
     console.log(process.env.REACT_APP_API_URL)
 
     // eslint-disable-next-line
