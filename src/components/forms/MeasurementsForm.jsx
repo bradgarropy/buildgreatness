dea2a387a8f418dad08919b14032b7e8fe4b15d0
Brad Garropy <bradgarropy@gmail.com>
2018-03-15 21:12:35 -0500

    measurements form.

---
+++ src/components/forms/MeasurementsForm.jsx
@@ -0,0 +1,133 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// components
+import NumberInput from "../inputs/NumberInput"
+import DateInput from "../inputs/DateInput"
+
+// material ui
+import RaisedButton from "material-ui/RaisedButton"
+
+// styles
+import "../../css/MeasurementsForm.css"
+
+
+const MeasurementsForm = (props) => (
+
+    <div className="measurements-form">
+
+        <form className="measurements-form-form" onSubmit={this.onSubmit}>
+
+            <DateInput
+                label="Date"
+                name="date"
+                value={props.measurement.date}
+                error={props.errors.date}
+                onChange={props.onChange}
+            />
+
+            <div className="measurements-form-inputs">
+
+                <div className="measurements-form-group">
+
+                    <h2>Composition</h2>
+
+                    <NumberInput
+                        label="Weight"
+                        name="weight"
+                        value={props.measurement.weight}
+                        error={props.errors.weight}
+                        onChange={props.onChange}
+                    />
+
+                    <NumberInput
+                        label="Body Fat"
+                        name="body_fat"
+                        value={props.measurement.body_fat}
+                        error={props.errors.body_fat}
+                        onChange={props.onChange}
+                    />
+
+                </div>
+
+                <div className="measurements-form-group">
+
+                    <h2>Measurements</h2>
+
+                    <NumberInput
+                        label="Bicep"
+                        name="bicep"
+                        value={props.measurement.bicep}
+                        error={props.errors.bicep}
+                        onChange={props.onChange}
+                    />
+
+                    <NumberInput
+                        label="Calf"
+                        name="calf"
+                        value={props.measurement.calf}
+                        error={props.errors.calf}
+                        onChange={props.onChange}
+                    />
+
+                    <NumberInput
+                        label="Chest"
+                        name="chest"
+                        value={props.measurement.chest}
+                        error={props.errors.chest}
+                        onChange={props.onChange}
+                    />
+
+                    <NumberInput
+                        label="Quad"
+                        name="quad"
+                        value={props.measurement.quad}
+                        error={props.errors.quad}
+                        onChange={props.onChange}
+                    />
+
+                    <NumberInput
+                        label="Shoulder"
+                        name="shoulder"
+                        value={props.measurement.shoulder}
+                        error={props.errors.shoulder}
+                        onChange={props.onChange}
+                    />
+
+                    <NumberInput
+                        label="Waist"
+                        name="waist"
+                        value={props.measurement.waist}
+                        error={props.errors.waist}
+                        onChange={props.onChange}
+                    />
+
+                </div>
+
+            </div>
+
+            <RaisedButton
+                className="measurements-form-button"
+                type="submit"
+                primary
+                label="submit"
+                onClick={props.onSubmit}
+            />
+
+        </form>
+
+    </div>
+
+)
+
+
+MeasurementsForm.propTypes = {
+    measurement: PropTypes.object,
+    errors: PropTypes.object,
+    onChange: PropTypes.func,
+    onSubmit: PropTypes.func,
+}
+
+
+// export
+export default MeasurementsForm
--- src/components/views/Measurements.jsx
+++ src/components/views/Measurements.jsx
@@ -1,5 +1,10 @@
 import React from "react"
-import PropTypes from "prop-types"
+
+// components
+import MeasurementsForm from "../forms/MeasurementsForm"
+
+// api
+import measurements from "../../api/measurements"
 
 
 class Measurements extends React.Component {
@@ -8,9 +13,62 @@ class Measurements extends React.Component {
 
         super(props)
 
-        this.state = {
+        const date = new Date()
+        date.setHours(0, 0, 0, 0)
 
+        this.state = {
+            errors: {},
+            measurement: {
+                body_fat: "",
+                bicep: "",
+                calf: "",
+                chest: "",
+                date,
+                quad: "",
+                shoulder: "",
+                waist: "",
+                weight: "",
+            },
         }
+
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
+    }
+
+    onChange(name, value) {
+
+        let measurement = this.state.measurement
+        measurement[name] = value
+
+        this.setState({measurement})
+
+    }
+
+    onSubmit(event) {
+
+        event.preventDefault()
+
+        let measurement = Object.assign({}, this.state.measurement)
+
+        Object.keys(measurement).forEach((key) => {
+
+            if(!measurement[key]) {
+                delete measurement[key]
+            }
+
+        })
+
+        measurements.add(measurement)
+            .then((response) => {
+                console.log(response.data)
+            })
+            .catch((error) => {
+
+                const errors = error.response.data
+                this.setState({errors})
+
+            })
 
     }
 
@@ -19,22 +77,19 @@ class Measurements extends React.Component {
         return (
 
             <div className="measurements">
-                measurements
+
+                <MeasurementsForm
+                    measurement={this.state.measurement}
+                    errors={this.state.errors}
+                    onChange={this.onChange}
+                    onSubmit={this.onSubmit}
+                />
+
             </div>
 
         )
 
     }
-
-}
-
-
-Measurements.propTypes = {
-
-}
-
-
-Measurements.defaultProps = {
 
 }
 
